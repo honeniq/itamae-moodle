651c1437def845a00ba8ac041e1e4be1d5332a2d
honeniq <honeniq@gmail.com>
2015-05-24 02:22:38 +0900

    Implement moodle spec

---
+++ spec/moodle/moodle_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "nginx" do
+  describe package('nginx') do
+    it { should be_installed }
+  end
+  
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+  
+  describe port(80) do
+    it { should be_listening }
+  end
+end
+
+describe "PHP modules" do
+  %w{php5-common php5-curl php5-fpm php5-gd php5-intl php5-json php5-pgsql php5-xmlrpc}.each do | pkg |
+    describe package(pkg) do
+      it { should be_installed }
+    end
+  end
+end
